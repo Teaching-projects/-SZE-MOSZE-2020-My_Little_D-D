147f567cecd594e9eee3baf30e4f4956d2d95754
RaboczkiCsongor <csongor@raboczki.hu>
2020-09-15 19:28:35 +0200

    Fixing main
    No pointers, const print function

--- main.cpp
+++ main.cpp
@@ -6,43 +6,42 @@
 
 #include "Unit.h"
 
-void printUnits(Unit* UnitA, Unit* UnitB){
-    std::cout << UnitA->Unit::getName() << " Hp:" << UnitA->Unit::getHealth() << " Damage: " << UnitA->Unit::getDamage() << std::endl;
-    std::cout << UnitB->Unit::getName() << " Hp:" << UnitB->Unit::getHealth() << " Damage: " << UnitB->Unit::getDamage() << std::endl;
+void const printUnits(Unit UnitA, Unit UnitB) {
+    std::cout << UnitA.getName() << " Hp:" << UnitA.getHealth() << " Damage: " << UnitA.getDamage() << std::endl;
+    std::cout << UnitB.getName() << " Hp:" << UnitB.getHealth() << " Damage: " << UnitB.getDamage() << std::endl;
 }
 
-void fight(Unit* UnitA, Unit* UnitB){
+void fight(Unit& UnitA, Unit& UnitB){
     printUnits(UnitA,UnitB);
-    while(UnitA->Unit::getHealth() > 0 && UnitB->Unit::getHealth() > 0){
-        std::cout << UnitA->getName() << " -> " << UnitB->getName() << std::endl; 
-        UnitB->getAttacked(UnitA->getDamage());
+    while(UnitA.getHealth() > 0 && UnitB.getHealth() > 0){
+        std::cout << UnitA.getName() << " -> " << UnitB.getName() << std::endl; 
+        UnitB.getAttacked(UnitA.getDamage());
         printUnits(UnitA,UnitB);
-        if(UnitB->getHealth()<=0){
+        if(UnitB.getHealth()<=0){
             break;
         }
-        std::cout << UnitB->getName() << " -> " << UnitA->getName() << std::endl;
-        UnitA->getAttacked(UnitB->getDamage());
+        std::cout << UnitB.getName() << " -> " << UnitA.getName() << std::endl;
+        UnitA.getAttacked(UnitB.getDamage());
         printUnits(UnitA,UnitB);
-        if(UnitA->getHealth()<=0){
+        if(UnitA.getHealth()<=0){
             break;
         }
     }
-    if(UnitA->getHealth()<=0){
-        std::cout << UnitA->getName() << " died. " << UnitB->getName() << " wins. " << std::endl;
+    if(UnitA.getHealth()<=0){
+        std::cout << UnitA.getName() << " died. " << UnitB.getName() << " wins. " << std::endl;
     }
-    if(UnitB->getHealth()<=0){
-        std::cout << UnitB->getName() << " died. " << UnitA->getName() << " wins. " << std::endl;
+    if(UnitB.getHealth()<=0){
+        std::cout << UnitB.getName() << " died. " << UnitA.getName() << " wins. " << std::endl;
     }
 }
 
 int main(int argc, char** argv){
-    Unit* UnitOne = new Unit(argv[1],argv[2],argv[3]);
-    Unit* UnitTwo = new Unit(argv[4],argv[5],argv[6]);
+    //Unit* UnitOne = new Unit(argv[1],argv[2],argv[3]);
+   // Unit* UnitTwo = new Unit(argv[4],argv[5],argv[6]);
 
+    Unit UnitOne(argv[1],argv[2],argv[3]);
+    Unit UnitTwo(argv[4],argv[5],argv[6]);
     fight(UnitOne,UnitTwo);
-    
 
-    delete UnitOne;
-    delete UnitTwo;
     return 0;
 } 
